de40cca4f1de5a7ad7534c316ef759e08d47d84a
deepanshut041 <deepanshut041@gmail.com>
2019-02-26 18:13:03 +0530

    Added new design for android

---
+++ app/src/main/java/in/saeakgec/ebike/fragment/main/HistoryFragment.java
@@ -0,0 +1,30 @@
+package in.saeakgec.ebike.fragment.main;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.saeakgec.ebike.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HistoryFragment extends Fragment {
+
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+}
